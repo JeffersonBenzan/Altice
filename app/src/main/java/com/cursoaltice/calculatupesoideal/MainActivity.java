56e3b805fc15b07bdd30b552519ac61624d577b4
Jefferson Benzan Tapia <Jeffersonbt20@gmail.com>
2018-03-10 05:04:53 -0400

    Created choose method screen

--- app/src/main/java/com/cursoaltice/calculatupesoideal/MainActivity.java
+++ app/src/main/java/com/cursoaltice/calculatupesoideal/MainActivity.java
@@ -88,9 +88,9 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_help) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
 
         }
 
